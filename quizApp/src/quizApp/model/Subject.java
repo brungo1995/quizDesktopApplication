6590dfef5647893ed59fb0ff30269497b678735d
Elie Omatuku <eomatuku@yahoo.fr>
2019-05-02 21:48:53 +0200

    Added attributes to models

--- quizApp/src/quizApp/model/Subject.java
+++ quizApp/src/quizApp/model/Subject.java
@@ -1,5 +1,28 @@
 package quizApp.model;
 
 public class Subject {
-
+	private String subjectName;
+	private int subjectCode; 
+	
+	public Subject(String subjectName, int subjectCode) {
+		this.subjectCode = subjectCode;
+		this.subjectName = subjectName;
+    }
+	
+	
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+	
+	public String getSubjectName() {
+		return subjectName;
+	}
+	
+	public void setSubjectCode(int subjectCode) {
+		this.subjectCode = subjectCode;
+	}
+	
+	public int getSubjectCode() {
+		return subjectCode;
+	}
 }
--- quizApp/src/quizApp/model/User.java
+++ quizApp/src/quizApp/model/User.java
@@ -1,5 +1,33 @@
 package quizApp.model;
 
 public class User {
-
+	private int userId;
+	private String name;
+	private int score;
+	
+	public User(int userId, String name, int score) {
+		this.userId = userId;
+		this.name = name;
+		this.score = score;
+	}
+	
+	public int getUserId () {
+		return userId;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int getScore () {
+		return score;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName () {
+		return name;
+	}
 }
